29eaadcc95b118fc55afc11ad56fee0712f0ccaf
PollitoDeColores <santiago.banol@upb.edu.co>
2022-11-17 10:35:36 -0500

    Funcional

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace VisualCoachPrototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SharpLeftBtn = new System.Windows.Forms.Button();
+            this.NormalLeftBtn = new System.Windows.Forms.Button();
+            this.LeftBtn = new System.Windows.Forms.Button();
+            this.RightBtn = new System.Windows.Forms.Button();
+            this.NormalRightBtn = new System.Windows.Forms.Button();
+            this.SharpRightBtn = new System.Windows.Forms.Button();
+            this.LeftWarning = new System.Windows.Forms.PictureBox();
+            this.RightWarning = new System.Windows.Forms.PictureBox();
+            this.WOTBtn = new System.Windows.Forms.Button();
+            this.BrakeBtn = new System.Windows.Forms.Button();
+            this.BrakeWarning = new System.Windows.Forms.PictureBox();
+            this.WOTWarning = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.LeftWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrakeWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WOTWarning)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SharpLeftBtn
+            // 
+            this.SharpLeftBtn.BackColor = System.Drawing.Color.White;
+            this.SharpLeftBtn.FlatAppearance.BorderSize = 0;
+            this.SharpLeftBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SharpLeftBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.SharpLeftBtn.Location = new System.Drawing.Point(10, 12);
+            this.SharpLeftBtn.Name = "SharpLeftBtn";
+            this.SharpLeftBtn.Size = new System.Drawing.Size(173, 231);
+            this.SharpLeftBtn.TabIndex = 0;
+            this.SharpLeftBtn.UseVisualStyleBackColor = false;
+            this.SharpLeftBtn.Click += new System.EventHandler(this.SharpLeftBtn_Click);
+            // 
+            // NormalLeftBtn
+            // 
+            this.NormalLeftBtn.BackColor = System.Drawing.Color.White;
+            this.NormalLeftBtn.FlatAppearance.BorderSize = 0;
+            this.NormalLeftBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NormalLeftBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.NormalLeftBtn.Location = new System.Drawing.Point(10, 288);
+            this.NormalLeftBtn.Name = "NormalLeftBtn";
+            this.NormalLeftBtn.Size = new System.Drawing.Size(173, 86);
+            this.NormalLeftBtn.TabIndex = 1;
+            this.NormalLeftBtn.UseVisualStyleBackColor = false;
+            this.NormalLeftBtn.Click += new System.EventHandler(this.NormalLeftBtn_Click);
+            // 
+            // LeftBtn
+            // 
+            this.LeftBtn.BackColor = System.Drawing.Color.White;
+            this.LeftBtn.FlatAppearance.BorderSize = 0;
+            this.LeftBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LeftBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.LeftBtn.Location = new System.Drawing.Point(10, 422);
+            this.LeftBtn.Name = "LeftBtn";
+            this.LeftBtn.Size = new System.Drawing.Size(173, 194);
+            this.LeftBtn.TabIndex = 2;
+            this.LeftBtn.UseVisualStyleBackColor = false;
+            this.LeftBtn.Click += new System.EventHandler(this.LeftBtn_Click);
+            // 
+            // RightBtn
+            // 
+            this.RightBtn.BackColor = System.Drawing.Color.White;
+            this.RightBtn.FlatAppearance.BorderSize = 0;
+            this.RightBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RightBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.RightBtn.Location = new System.Drawing.Point(495, 422);
+            this.RightBtn.Name = "RightBtn";
+            this.RightBtn.Size = new System.Drawing.Size(168, 194);
+            this.RightBtn.TabIndex = 5;
+            this.RightBtn.UseVisualStyleBackColor = false;
+            this.RightBtn.Click += new System.EventHandler(this.RightBtn_Click);
+            // 
+            // NormalRightBtn
+            // 
+            this.NormalRightBtn.BackColor = System.Drawing.Color.White;
+            this.NormalRightBtn.FlatAppearance.BorderSize = 0;
+            this.NormalRightBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NormalRightBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.NormalRightBtn.Location = new System.Drawing.Point(495, 288);
+            this.NormalRightBtn.Name = "NormalRightBtn";
+            this.NormalRightBtn.Size = new System.Drawing.Size(168, 86);
+            this.NormalRightBtn.TabIndex = 4;
+            this.NormalRightBtn.UseVisualStyleBackColor = false;
+            this.NormalRightBtn.Click += new System.EventHandler(this.NormalRightBtn_Click);
+            // 
+            // SharpRightBtn
+            // 
+            this.SharpRightBtn.BackColor = System.Drawing.Color.White;
+            this.SharpRightBtn.FlatAppearance.BorderSize = 0;
+            this.SharpRightBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SharpRightBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.SharpRightBtn.Location = new System.Drawing.Point(495, 12);
+            this.SharpRightBtn.Name = "SharpRightBtn";
+            this.SharpRightBtn.Size = new System.Drawing.Size(168, 231);
+            this.SharpRightBtn.TabIndex = 3;
+            this.SharpRightBtn.UseVisualStyleBackColor = false;
+            this.SharpRightBtn.Click += new System.EventHandler(this.SharpRightBtn_Click);
+            // 
+            // LeftWarning
+            // 
+            this.LeftWarning.BackColor = System.Drawing.Color.White;
+            this.LeftWarning.Location = new System.Drawing.Point(-4, -3);
+            this.LeftWarning.Name = "LeftWarning";
+            this.LeftWarning.Size = new System.Drawing.Size(341, 638);
+            this.LeftWarning.TabIndex = 6;
+            this.LeftWarning.TabStop = false;
+            this.LeftWarning.Click += new System.EventHandler(this.LeftWarning_Click);
+            // 
+            // RightWarning
+            // 
+            this.RightWarning.BackColor = System.Drawing.Color.White;
+            this.RightWarning.Location = new System.Drawing.Point(333, -3);
+            this.RightWarning.Name = "RightWarning";
+            this.RightWarning.Size = new System.Drawing.Size(348, 638);
+            this.RightWarning.TabIndex = 7;
+            this.RightWarning.TabStop = false;
+            // 
+            // WOTBtn
+            // 
+            this.WOTBtn.BackColor = System.Drawing.Color.White;
+            this.WOTBtn.FlatAppearance.BorderSize = 0;
+            this.WOTBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.WOTBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.WOTBtn.Location = new System.Drawing.Point(354, 12);
+            this.WOTBtn.Name = "WOTBtn";
+            this.WOTBtn.Size = new System.Drawing.Size(75, 231);
+            this.WOTBtn.TabIndex = 8;
+            this.WOTBtn.UseVisualStyleBackColor = false;
+            this.WOTBtn.Click += new System.EventHandler(this.WOTBtn_Click);
+            // 
+            // BrakeBtn
+            // 
+            this.BrakeBtn.BackColor = System.Drawing.Color.White;
+            this.BrakeBtn.FlatAppearance.BorderSize = 0;
+            this.BrakeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BrakeBtn.ForeColor = System.Drawing.Color.Transparent;
+            this.BrakeBtn.Location = new System.Drawing.Point(261, 422);
+            this.BrakeBtn.Name = "BrakeBtn";
+            this.BrakeBtn.Size = new System.Drawing.Size(66, 194);
+            this.BrakeBtn.TabIndex = 9;
+            this.BrakeBtn.UseVisualStyleBackColor = false;
+            this.BrakeBtn.Click += new System.EventHandler(this.BrakeBtn_Click);
+            // 
+            // BrakeWarning
+            // 
+            this.BrakeWarning.BackColor = System.Drawing.Color.White;
+            this.BrakeWarning.Location = new System.Drawing.Point(-4, 380);
+            this.BrakeWarning.Name = "BrakeWarning";
+            this.BrakeWarning.Size = new System.Drawing.Size(685, 255);
+            this.BrakeWarning.TabIndex = 10;
+            this.BrakeWarning.TabStop = false;
+            // 
+            // WOTWarning
+            // 
+            this.WOTWarning.BackColor = System.Drawing.Color.White;
+            this.WOTWarning.Location = new System.Drawing.Point(-4, -3);
+            this.WOTWarning.Name = "WOTWarning";
+            this.WOTWarning.Size = new System.Drawing.Size(685, 285);
+            this.WOTWarning.TabIndex = 11;
+            this.WOTWarning.TabStop = false;
+            this.WOTWarning.Click += new System.EventHandler(this.WOTWarning_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(271, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 86);
+            this.button1.TabIndex = 12;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(675, 628);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.WOTBtn);
+            this.Controls.Add(this.BrakeBtn);
+            this.Controls.Add(this.LeftBtn);
+            this.Controls.Add(this.RightBtn);
+            this.Controls.Add(this.NormalRightBtn);
+            this.Controls.Add(this.SharpRightBtn);
+            this.Controls.Add(this.NormalLeftBtn);
+            this.Controls.Add(this.SharpLeftBtn);
+            this.Controls.Add(this.WOTWarning);
+            this.Controls.Add(this.LeftWarning);
+            this.Controls.Add(this.BrakeWarning);
+            this.Controls.Add(this.RightWarning);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.LeftWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RightWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrakeWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WOTWarning)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SharpLeftBtn;
+        private System.Windows.Forms.Button NormalLeftBtn;
+        private System.Windows.Forms.Button LeftBtn;
+        private System.Windows.Forms.Button RightBtn;
+        private System.Windows.Forms.Button NormalRightBtn;
+        private System.Windows.Forms.Button SharpRightBtn;
+        private System.Windows.Forms.PictureBox LeftWarning;
+        private System.Windows.Forms.PictureBox RightWarning;
+        private System.Windows.Forms.Button WOTBtn;
+        private System.Windows.Forms.Button BrakeBtn;
+        private System.Windows.Forms.PictureBox BrakeWarning;
+        private System.Windows.Forms.PictureBox WOTWarning;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace VisualCoachPrototype
+{
+    public partial class Form1 : Form
+    {
+        Color warningColor = new Color();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*BrakeBtn.Visible = false;
+            LeftBtn.Visible = false;
+            NormalLeftBtn.Visible = false;
+            NormalRightBtn.Visible = false;
+            RightBtn.Visible = false;
+            SharpLeftBtn.Visible = false;
+            SharpRightBtn.Visible = false;
+            WOTBtn.Visible = false;
+            warningColor = Color.Empty;*/
+        }
+        public void warningSender(Color color, int side)
+        {
+            switch (side)
+            {
+                case 1:
+                    LeftWarning.Visible = true;
+                    BrakeWarning.Visible = false;
+                    WOTWarning.Visible = false;
+                    RightWarning.Visible = false;
+                    LeftWarning.BackColor = color;
+                    LeftBtn.BackColor = color;
+                    SharpLeftBtn.BackColor = color;
+                    NormalLeftBtn.BackColor = color;
+                    BrakeBtn.BackColor = color;
+                    break;
+
+                case 2:
+                    LeftWarning.Visible = false;
+                    BrakeWarning.Visible = false;
+                    WOTWarning.Visible = false;
+                    RightWarning.Visible = true;
+                    RightWarning.BackColor = color;
+                    RightBtn.BackColor = color;
+                    SharpRightBtn.BackColor = color;
+                    NormalRightBtn.BackColor = color;
+                    WOTBtn.BackColor = color;
+                    break;
+
+                case 3:
+                    LeftWarning.Visible = false;
+                    BrakeWarning.Visible = false;
+                    WOTWarning.Visible = true;
+                    RightWarning.Visible = false;
+                    WOTWarning.BackColor = color;
+                    WOTBtn.BackColor = color;
+                    SharpRightBtn.BackColor = color;
+                    SharpLeftBtn.BackColor = color;
+                    break;
+
+                case 4:
+                    LeftWarning.Visible = false;
+                    BrakeWarning.Visible = true;
+                    WOTWarning.Visible = false;
+                    RightWarning.Visible = false;
+                    BrakeWarning.BackColor = color;
+                    BrakeBtn.BackColor = color;
+                    RightBtn.BackColor = color;
+                    LeftBtn.BackColor = color;
+                    break;
+
+            }
+        }
+
+        private void SharpLeftBtn_Click(object sender, EventArgs e)
+        {
+            warningColor = Color.Red;
+            warningSender(warningColor, 1);
+        }
+
+        private void SharpRightBtn_Click(object sender, EventArgs e)
+        {
+            warningColor = Color.Red;
+            warningSender(warningColor, 2);
+        }
+
+        private void NormalLeftBtn_Click(object sender, EventArgs e)
+        {
+            warningColor = Color.YellowGreen;
+            warningSender(warningColor, 1);
+        }
+
+        private void NormalRightBtn_Click(object sender, EventArgs e)
+        {
+            warningColor = Color.YellowGreen;
+            warningSender(warningColor, 2);
+        }
+
+        private void LeftBtn_Click(object sender, EventArgs e)
+        {
+            warningColor = Color.SkyBlue;
+            warningSender(warningColor, 1);
+        }
+
+        private void RightBtn_Click(object sender, EventArgs e)
+        {
+            warningColor = Color.SkyBlue;
+            warningSender(warningColor, 2);
+        }
+
+        private void WOTBtn_Click(object sender, EventArgs e)
+        {
+            warningColor = Color.DarkCyan;
+            warningSender(warningColor, 3);
+        }
+
+        private void BrakeBtn_Click(object sender, EventArgs e)
+        {
+            warningColor = Color.Crimson;
+            warningSender(warningColor, 4);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            warningColor = Color.White;
+            LeftWarning.BackColor = warningColor;
+            LeftBtn.BackColor = warningColor;
+            SharpLeftBtn.BackColor = warningColor;
+            NormalLeftBtn.BackColor = warningColor;
+            RightWarning.BackColor = warningColor;
+            RightBtn.BackColor = warningColor;
+            SharpRightBtn.BackColor = warningColor;
+            NormalRightBtn.BackColor = warningColor;
+            WOTBtn.BackColor = warningColor;
+            BrakeBtn.BackColor = warningColor;
+            BrakeWarning.BackColor = warningColor;
+            WOTWarning.BackColor = warningColor;
+        }
+
+    }
+}
